fc4e38384a137ce3e0d37823f81a1869417532ba
CamCitizen <35842314+CamCitizen@users.noreply.github.com>
2023-06-06 11:02:55 -0600

    Update and rename shadercacheclean_1.0.2.bat to shadercacheclean_1.0.5.bat
    Updated code to show file count within the Shader Cache folder

--- shadercacheclean_1.0.5.bat
+++ shadercacheclean_1.0.5.bat
@@ -1,16 +1,22 @@
 @echo off
-rem This is a custom batch script for the Star Citizen Shade Cache folder.
+rem This is a custom batch script for the Star Citizen Shader Cache folder.
 
 echo Welcome to the Star Citizen Shader Cache Cleaner by CamCitizen!
-echo Version 1.0.2
+echo Version 1.0.5
+echo.
+echo.
 
 rem Display the contents of the Star Citizen Shader Cache folder.
 set SC_DIR=%localappdata%\Star Citizen
-echo The contents of the Star Citizen Shader Cache folder:
-dir "%SC_DIR%"
-
+echo The contents of the Star Citizen Shader Cache folder (%localappdata%\Star Citizen):
+dir /b "%SC_DIR%"
+echo ------
+set SC_FILES=%localappdata%\Star Citizen
+@for /f %%a in ('2^>nul dir "%SC_FILES%" /a-d/b/-o/-p/s^|find /v /c ""') do set n=%%a
+@echo Total files: %n%!
 echo.
-echo Do you want to delete all files within the Star Citizen Shader Cache folder? (Y/N)
+
+echo Do you want to delete all files within the Star Citizen Shader Cache folder?
 choice /C YN /M "Please enter your choice:"
 if %errorlevel% equ 1 (
     rem Count the number of files before deletion.
